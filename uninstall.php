ec8e96f6688370501e36bfdfe19cc219b8464d85
Dimitris <simplesteps.info+222@gmail.com>
2022-10-06 18:12:43 +0300

    Added correct option to uninstall

--- uninstall.php
+++ uninstall.php
@@ -1,9 +1,10 @@
 <?php
-//if uninstall/delete not called from WordPress exit
+/**
+ * If uninstall/delete not called from WordPress exit
+ */
+
 if( ! defined( 'ABSPATH' ) && ! defined( 'WP_UNINSTALL_PLUGIN' ) )
     exit ();
 // Delete options array from options table
-delete_option( 'number_of_posts' );
-delete_option( 'post_category' );
-delete_option( 'post_date' );
+delete_option('pbd_plugin_options');
 ?>
